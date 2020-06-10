6baba26d02335e674a122efa7795630f0fb1a2c0
Justin Hamilton <JHamilton@Pas.com>
2020-06-10 16:47:44 -0500

    Add project files.

---
+++ Pages/Models/IData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActorsRazorPages.Pages.Models
+{
+    public interface IData
+    {
+        public List<Actor> ActorsList { get; set; }
+        public List<Actor> ActorsInitializeData();
+        public Actor GetActorById(int? id);
+    }
+}
